24cfe4a110290b6e426a4003b0abaac7b22cdeef
Lars314 <larsborchert314@gmail.com>
2025-08-01 15:08:49 +0200

    restructured buffer

--- Interface/controlGUI.py
+++ Interface/controlGUI.py
@@ -176,7 +176,7 @@ class TimescanPlot():
             y1 = 'Sample T (K)'
             #y1Data = [row[y1] for row in data]
             y1Data = list(data[y1])
-            print(y1Data)
+            print(xData)
             self.data_line1.setData(xData, y1Data,
                                     pen=self.yItems[y1]['pen'], name=y1)
             self.figureLegend.addItem(self.data_line1, y1)
